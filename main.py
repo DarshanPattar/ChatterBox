b1c6a41735c20787185fa3960bbe2fc1380af2cb
Darshan <darshanbpattar@gmail.com>
2024-01-12 22:11:04 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, session, redirect, request
+import pymysql
+
+from userClass import User
+
+
+
+db = pymysql.connect(host = "localhost", user = "root", password = "1234", db = "twitter_clone", autocommit = True)
+cursor = db.cursor()
+
+app = Flask(__name__)
+
+app.secret_key = "hello"
+
+@app.route("/")
+def index():
+    if 'username' in session:
+        username = session['username']
+        sql = f"select name from users where username='{username}';"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+
+        return render_template('html/index.html', name='index', result = result)
+    return redirect("/signup")
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "POST":
+        session['username'] = request.form['username']
+        username = request.form['username']
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['pass1']
+        # Cpassword = request.form['pass2']
+
+        # sql = f'''insert into users values(id, '{username}', '{name}', '{email}', '{password}', '', 0);'''
+        # cursor.execute(sql)
+
+        user = User(username=username, name=name, email=email, password=password)
+        if(not user.Authenticate()):
+            user.CreateUser()   
+
+
+        
+
+        return redirect('/')
+    return render_template('html/signup.html', name='signup')    
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/signup')
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ userClass.py
@@ -0,0 +1,77 @@
+import pymysql
+
+db = pymysql.connect(host = "localhost", user = "root", password = "1234", db = "twitter_clone", autocommit=True)
+cursor = db.cursor()
+
+
+class User:
+    def __init__(self, username, name, email, password, bio = ""):
+        self.username = username
+        self.name = name
+        self.email = email
+        self.password = password
+        self.bio = bio
+    
+    def Authenticate(self):
+        sql = f'''select * from users where username='{self.username}';'''
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        if result:
+            print(f"Connected, User exist, user {result}")
+            self.id = result[0]
+            return self.id
+        else:
+            print("User does not exists!")
+            return False
+    
+    def AuthenticateByUsername(self, username):
+        sql = f'''select * from users where username='{username}';'''
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        if result:
+            print(f"Connected, User exist, user {result}")
+            self.id = result[0]
+            self.username = result[1]
+            self.name = result[2]
+            self.email = result[3]
+            self.password = result[4]
+            self.bio = result[5]
+            return self.id
+        else:
+            print("User does not exists!")
+            return False
+        
+    def AuthenticateById(self, id):
+        sql = f'''select * from users where id={id};'''
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        if result:
+            print(f"Connected, User exist, user {result}")
+            self.id = result[0]
+            self.username = result[1]
+            self.name = result[2]
+            self.email = result[3]
+            self.password = result[4]
+            self.bio = result[5]
+            return self.id
+        else:
+            print("User does not exists!")
+            return False
+
+
+
+    def CreateUser(self):
+        # insert into users values(id, 'dbpattar', 'Darshan B Pattar', 'darshanbpattar@gmail.com', 'qwerty123', 'This is super user!', 1);
+        if(self.Authenticate() == False):
+            sql = f'''insert into users values(id, '{self.username}', '{self.name}', '{self.email}', '{self.password}', '{self.bio}', 0);'''
+            cursor.execute(sql)
+            if self.Authenticate():
+                print(f"Successfully created User, user id = {self.Authenticate()}")
+                return self.id
+            else:
+                print("Failed to create User")
+                return False
+        else:
+            return False
+
+    
